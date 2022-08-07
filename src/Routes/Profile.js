dd455b2b529e1b4499f5aa6b5ca93e83e211b676
originalkyu <pdpdkyu@gmail.com>
2022-08-07 22:13:53 +0900

    progile.js

--- src/Routes/Profile.js
+++ src/Routes/Profile.js
@@ -1,95 +1,187 @@
 import React, {useState} from "react";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
-//import Footer from "../src/components/Footer.js"
+import Footer from "../components/Footer"
 import PopUp from "../components/PopUp";
 
 
 
+const SelectionStatus = {
+    POSTS : "posts",
+    SAVED : "saved",
+    TAGGED : "tagged"
+}
+
+const borderInvalid = css`
+`
+const borderValid = css`
+  border-top: 1px solid #000;
+  color: #000;
+`
+
+
 const Wrapper = styled.div`
   background-color: #fafafa;
-  width: 100%;
-  height: 100vh;
-  padding: 10vh 0;
+  width: 100vw;
+  padding-top: 80px;
   margin: 0 auto;
 `;
 
 const Container = styled.div`
+  width: 90%;
+  max-width: 900px;
+  min-width: 750px;
+  
+
   box-sizing: border-box;
   padding-top: 10px;
   margin: 0 auto;
   position: relative;
-  width: 100%;
-  height: auto;
-  /* height: 100%; */
-  max-width: 900px;
-  min-width: 900px;
 
+  display: flex;
+  flex-direction: column;
+
+  //border: 1px solid #000;
 `;
 const ProfileBox = styled.div`
-  display: block;
-  width: 100%;
-  height: 280px;
-  background-color: ;
+    width: 100%;
+    height: 152px;
+    margin-bottom: 44px;
+    background-color: ;
+
+    display: flex;
 `;
 
 const ProfileImg = styled.div`
-  top: 400px;
-  width: 150px;
-  height: 150px;
-  margin-top: -50px;
-  margin-left: 00px; 
-  border-radius: 85px;
-  background-size: cover;
-  left: 50%;
-  transform: translate(50%, 50%);
-  text-align: center;
-  background-image: url("https://content.presspage.com/uploads/2379/1920_ocean-sunset-195865.jpg?10000");
+    flex: 0.1 0 150px;
+    margin-right: 30px;
+    & div {
+        width: 150px;
+        height: 150px;
+          
+        border-radius: 85px;
+        background-size: cover;
+        text-align: center;
+        background-image: url("https://content.presspage.com/uploads/2379/1920_ocean-sunset-195865.jpg?10000");
+    }
 `;
 
 const ProfileName = styled.div`
+    flex: 1 0 280px;
   word-spacing: 8px;
-  position: absolute;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  
+
+
   text-align: left;
-  top: 100px;
+  
   font-size: 27px;
   font-weight: 600;
   opacity: 0.6;
-  margin-top: 10px;
+  
   line-height : 170%;
 `;
 
+//
+const SelectionBox = styled.div`
+    width: 100%;
+    height: 50px;
+
+    display: flex;
+    justify-content: center;
+
+    border-top: 1.2px solid rgba(0, 0, 0, 0.2);
+
+    & div {
+        width: 57px;
+        height: 52px;
+    
+        margin-right: 60px;
+        // &:not(:last-of-type) {
+        &:last-of-type {
+            margin-right: 0px;
+        }
+
+        display: flex;
+        align-items: center;
+
+        cursor: pointer;
+
+        &:nth-child(1) {
+            ${({ status }) => status === "posts" && (borderValid)} 
+            ${({ status }) => status === SelectionStatus.POSTS && (borderInvalid)}
+        }
+        
+        &:nth-child(2) {
+            ${({ status }) => status === SelectionStatus.SAVED && borderValid}
+            ${({ status }) => status != SelectionStatus.SAVED && borderInvalid}
+        }
+        
+        &:nth-child(3) {
+            ${({ status }) => status === SelectionStatus.TAGGED && borderValid}
+            ${({ status }) => status != SelectionStatus.TAGGED && borderInvalid}
+            
+        }
+    }
+`
+
+const SelectionItem = styled.a`
+    font-size: 12px;
+    color: #8E8E8E;
+    vertical-align: center;
+    letter-spacing: 1px;
+    font-weight: 600;
+
+`
+
 const PostBox = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(30%, 1fr));
-  grid-auto-rows: 20px;
-  grid-gap: 40px;
+  width: 100%;
+  height: 70%;
   margin: 0 auto;
-  position: absolute;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  top: 420px;
-  width: 980px;
-  /* display: flex; */
-  height: 130%;
-  /* display: block; */
-  /* justify-content: center; */
-  padding: 50px 200px;
-  flex-wrap: wrap;
+  
+// text-align: center;
+
+/* display: flex; */
+/* justify-content: center; */
+// flex-wrap: wrap;
+
+//   display: grid;
+//   grid-template-columns: repeat(auto-fill, minmax(30%, 1fr));
+//   grid-auto-rows: 20px;
+//   grid-gap: 40px;
   
 `;
 
-const FeedImg = styled.div`
-  width: 300px;
-  height: 300px;
-  margin-left: 50px; 
-  background-size: cover;
-  left: 50%;
-  background-image: url("https://content.presspage.com/uploads/2379/1920_ocean-sunset-195865.jpg?10000");
+const FeedBox = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+
+    & div {
+        box-sizing: border-box;
+        width: 33.33333%;
+        //min-height: 100px;
+        margin-right:28px;
+
+        // &:last-of-type {
+        //     margin-right: 0px;
+        // }
+
+        &:nth-child(3n) {
+            margin-right: 0px;
+        }
+    }
+`
+
+const FeedImg = styled.img`
+  //background-size: cover;
+  //background-image: url("https://content.presspage.com/uploads/2379/1920_ocean-sunset-195865.jpg?10000");
+  content: url("https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Square_definition.svg/180px-Square_definition.svg.png");
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+
+  cursor: pointer;
 `;
 
 const FollowContainer = styled.div`
@@ -107,6 +199,7 @@ const PopUpStatus = {
 
 function Profile() {
 
+    const [pageSelection, setPageSelection] = useState("posts");
 // pop up
     const [editPopUp, setEditPopUp] = useState(false);
 
@@ -124,42 +217,69 @@ function Profile() {
     https://velog.io/@kbing14/React-%EB%AA%A8%EB%8B%AC%EC%B0%BD-%ED%8C%9D%EC%97%85%EC%8B%9C-%EB%92%B7%EB%B0%B0%EA%B2%BD-%EC%8A%A4%ED%81%AC%EB%A1%A4-%EB%A7%89%EA%B8%B0
 */
     }
-  return <div>
-    <Layout>
-      <Wrapper>
-        <Container>
-          <ProfileBox>
-            <ProfileImg/>
-            <ProfileName>
-              <span> utae1102 </span>
-                <Link to = {"/ProfileChange"}>
-                 <button  style={{fontSize: "15px"}}> 프로필 편집 </button>
-                </Link>
-              <span onClick={() => {handleEditPopUp(true)}}> ⚙︎ </span>
-              {editPopUp && <PopUp onClose={handleEditPopUp} />}
-            <FollowContainer>
-              <span> 게시물5 </span>
-              <span> 팔로워100 </span>
-              <span> 팔로우100 </span>
-            </FollowContainer>
-              <p style={{ fontSize: "18px"}}>김우태</p> 
-            </ProfileName>
-          </ProfileBox>
-              <hr style={{ marginTop: "0", width: "100%", zIndex: "100" }}></hr>
-            <PostBox>
-              <p>게시물</p>
-              <p>저장됨</p>
-              <p>태그됨</p>
-              <FeedImg/>
-            </PostBox>
+
+    const handlePageSelection = (e) => {
+        // pageSelection을 posts로 바꾼다
+        if(e.target.textContent === "게시물")
+            setPageSelection("posts");
+        else if(e.target.textContent === "저장됨")
+            setPageSelection("saved");
+        else if(e.target.textContent === "태그됨")
+            setPageSelection("tagged");
+    }
+
+    return (<Layout>
+
+        <Wrapper id="Wrapper">
+            <Container id="Container">
+                <ProfileBox>
+                    <ProfileImg><div></div></ProfileImg>
+                    <ProfileName>
+                        <span> utae1102 </span>
+                        <Link to = {"/ProfileChange"}>
+                            <button  style={{fontSize: "15px"}}> 프로필 편집 </button>
+                        </Link>
+                        <span onClick={() => {handleEditPopUp(true)}}> ⚙︎ </span>
+                            {editPopUp && <PopUp onClose={handleEditPopUp} />}
+                        <FollowContainer>
+                            <span> 게시물5 </span>
+                            <span> 팔로워100 </span>
+                            <span> 팔로우100 </span>
+                        </FollowContainer>
+                        <p style={{ fontSize: "18px"}}>김우태</p> 
+                    </ProfileName>
+                </ProfileBox>
+                <SelectionBox>
+                    <div status={pageSelection}><SelectionItem onClick={handlePageSelection}>게시물</SelectionItem></div>
+                    <div status={pageSelection}><SelectionItem onClick={handlePageSelection}>저장됨</SelectionItem></div>
+                    <div status={pageSelection}><SelectionItem onClick={handlePageSelection}>태그됨</SelectionItem></div>
+                </SelectionBox>
+                {
+                    (pageSelection === SelectionStatus.POSTS)
+                    &&
+                        <PostBox>
+                            <FeedBox>
+                                <div><FeedImg/></div>
+                                <div><FeedImg/></div>
+                                <div><FeedImg/></div>
+                            </FeedBox>
+                            <FeedBox>
+                                <div><FeedImg/></div>
+                                <div><FeedImg/></div>
+                            </FeedBox>
+                            
+                        </PostBox>
+                    ||  (pageSelection === SelectionStatus.SAVED)
+                        && <div> saved </div>
+                    ||  (pageSelection === SelectionStatus.TAGGED)
+                        && <div> tagged </div>
+                }
         </Container> 
        </Wrapper>
-       {/* <Footer/> */}
-    </Layout>
-      
+       <Footer/>
+    </Layout>)
+    
    
-
-  </div>;
   
   
 }
