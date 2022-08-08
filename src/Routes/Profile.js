b422218674afbfa8d7bfa2173a10ece486484997
wootaes <109718366+wootaes@users.noreply.github.com>
2022-08-08 17:11:22 +0900

    profile 메인

--- src/Routes/Profile.js
+++ src/Routes/Profile.js
@@ -1,97 +1,204 @@
-import React, {useState} from "react";
-import styled, {css} from "styled-components";
+import React, { useState } from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
 import PopUp from "../components/PopUp";
 
-
-
 const SelectionStatus = {
-    POSTS : "posts",
-    SAVED : "saved",
-    TAGGED : "tagged"
+  POSTS : "posts",
+  SAVED : "saved",
+  TAGGED : "tagged"
 }
 
-const borderInvalid = css`
-`
-const borderValid = css`
-  border-top: 1px solid #000;
-  color: #000;
-`
+function Profile() {
+  // pop up
+  const [pageSelection, setPageSelection] = useState("posts");
+  const [editPopUp, setEditPopUp] = useState(false);
 
+  // pop up
+  const handleEditPopUp = (isEnable) => {
+    if (isEnable === true) {
+      setEditPopUp(true);
+      document.body.style.overflow = "hidden";
+    } else {
+      setEditPopUp(false);
+      document.body.style.overflow = "unset";
+    }
+   
+  };
+
+  const handlePageSelection = (e) => {
+    // pageSelection을 posts로 바꾼다
+    if(e.target.textContent === "게시물")
+        setPageSelection("posts");
+    else if(e.target.textContent === "저장됨")
+        setPageSelection("saved");
+    else if(e.target.textContent === "태그됨")
+        setPageSelection("tagged");
+}
+
+  return (
+    <div>
+      {editPopUp && <PopUp onClose={handleEditPopUp} />}
+      <Layout>
+        <Wrapper>
+          <Container>
+            <ProfileBox>
+              <ProfileImg />
+              <ProfileName> 
+                <span> utae1102 </span>
+                <Link to={"/ProfileChange"}>
+                  <button style={{ fontSize: "15px", cursor: "pointer", height: "27px", verticalAlign: "6px"}}> 프로필 편집 </button>
+                </Link>
+                <span style={{fontSize: "35px", cursor: "pointer", marginLeft: "15px"}}
+                  onClick={() => {
+                    handleEditPopUp(true);
+                  }}
+                >
+                  ⚙︎
+                </span>
+
+                <FollowContainer>
+                  <span> 게시물5 </span>
+                  <span> 팔로워100 </span>
+                  <span> 팔로우100 </span>
+                </FollowContainer>
+                <p style={{ fontSize: "18px" }}>김우태</p>
+              </ProfileName>
+            </ProfileBox>
+            <SelectionBox>
+                    <div status={pageSelection}><SelectionItem onClick={handlePageSelection}>게시물</SelectionItem></div>
+                    <div status={pageSelection}><SelectionItem onClick={handlePageSelection}>저장됨</SelectionItem></div>
+                    <div status={pageSelection}><SelectionItem onClick={handlePageSelection}>태그됨</SelectionItem></div>
+                </SelectionBox>
+                {
+                    ((pageSelection === SelectionStatus.POSTS)
+                    && (<PostBox>
+                      <p style={{margin: "30px 0 10px 0", fontSize: "12px", color: "gray"}}>
+                        저장한 내용은 회원님만 볼 수 있습니다. 
+                        <NewCollection>+ 새 컬렉션</NewCollection>
+                      </p>
+                  
+                      
+                            <FeedBox>
+                                <div><FeedImg/></div>
+                                <div><FeedImg/></div>
+                                <div><FeedImg/></div>
+                            </FeedBox>
+                            
+                        </PostBox>) )
+                    || (
+                    (pageSelection === SelectionStatus.SAVED)
+                    &&  <PostBox>
+                      <p style={{margin: "30px 0 10px 0", fontSize: "12px", color: "gray"}}>
+                        저장한 내용은 회원님만 볼 수 있습니다. 
+                        <NewCollection>+ 새 컬렉션</NewCollection>
+                      </p>
+                          <FeedBox>
+                               <div><FeedImg/></div>
+                               <div><FeedImg/></div>
+                                                      
+                          </FeedBox>
+                        </PostBox>
+                    )
+
+                    || (
+                    (pageSelection === SelectionStatus.TAGGED)
+                    && <PostBox>
+                     <p style={{margin: "30px 0 10px 0", fontSize: "12px", color: "gray"}}>
+                      저장한 내용은 회원님만 볼 수 있습니다. 
+                      <NewCollection>+ 새 컬렉션</NewCollection>
+                     </p>
+                        <FeedBox>
+                             <div><FeedImg/></div>
+                   
+                                                    
+                        </FeedBox>
+                      </PostBox>
+                    )
+                }
+            
+          </Container>
+          <Footer/>
+        </Wrapper>
+      </Layout>
+    </div>
+  );
+}
+
+export default Profile;
 
 const Wrapper = styled.div`
   background-color: #fafafa;
-  width: 100vw;
-  padding-top: 80px;
+  width: 100%;
+  height: 93vh;
+  padding: 10vh 0;
   margin: 0 auto;
 `;
 
 const Container = styled.div`
-  width: 90%;
-  max-width: 900px;
-  min-width: 750px;
-  
-
   box-sizing: border-box;
   padding-top: 10px;
   margin: 0 auto;
   position: relative;
-
-  display: flex;
-  flex-direction: column;
-
-  //border: 1px solid #000;
+  width: 100%;
+  height: auto;
+  /* height: 100%; */
+  max-width: 900px;
+  min-width: 900px;
 `;
 const ProfileBox = styled.div`
-    width: 100%;
-    height: 152px;
-    margin-bottom: 44px;
-    background-color: ;
-
-    display: flex;
+  display: block;
+  width: 100%;
+  height: 280px;
 `;
 
 const ProfileImg = styled.div`
-    flex: 0.1 0 150px;
-    margin-right: 30px;
-    & div {
-        width: 150px;
-        height: 150px;
-          
-        border-radius: 85px;
-        background-size: cover;
-        text-align: center;
-        background-image: url("https://content.presspage.com/uploads/2379/1920_ocean-sunset-195865.jpg?10000");
-    }
+  top: 400px;
+  width: 150px;
+  height: 150px;
+  margin-top: -50px;
+  margin-left: 00px;
+  border-radius: 85px;
+  background-size: cover;
+  left: 50%;
+  transform: translate(50%, 50%);
+  text-align: center;
+  background-image: url("https://content.presspage.com/uploads/2379/1920_ocean-sunset-195865.jpg?10000");
 `;
 
 const ProfileName = styled.div`
-    flex: 1 0 280px;
   word-spacing: 8px;
-  
-
-
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
   text-align: left;
-  
+  top: 100px;
   font-size: 27px;
   font-weight: 600;
   opacity: 0.6;
-  
-  line-height : 170%;
+  margin-top: 10px;
+  line-height: 170%;
 `;
 
-//
+const PostBox = styled.div`
+  width: 100%;
+  height: 70%;
+  margin: 0 auto;
+`;
+
+const FollowContainer = styled.div`
+  font-size: 18px;
+  word-spacing: 10px;
+`;
+
 const SelectionBox = styled.div`
     width: 100%;
     height: 50px;
-
     display: flex;
     justify-content: center;
-
     border-top: 1.2px solid rgba(0, 0, 0, 0.2);
-
     & div {
         width: 57px;
         height: 52px;
@@ -101,187 +208,61 @@ const SelectionBox = styled.div`
         &:last-of-type {
             margin-right: 0px;
         }
-
         display: flex;
         align-items: center;
-
         cursor: pointer;
-
         &:nth-child(1) {
-            ${({ status }) => status === "posts" && (borderValid)} 
-            ${({ status }) => status === SelectionStatus.POSTS && (borderInvalid)}
+            ${({ status }) => status === "posts" } 
+            ${({ status }) => status === SelectionStatus.POSTS}
         }
         
         &:nth-child(2) {
-            ${({ status }) => status === SelectionStatus.SAVED && borderValid}
-            ${({ status }) => status != SelectionStatus.SAVED && borderInvalid}
+            ${({ status }) => status === SelectionStatus.SAVED}
+            ${({ status }) => status !== SelectionStatus.SAVED}
         }
         
         &:nth-child(3) {
-            ${({ status }) => status === SelectionStatus.TAGGED && borderValid}
-            ${({ status }) => status != SelectionStatus.TAGGED && borderInvalid}
+            ${({ status }) => status === SelectionStatus.TAGGED}
+            ${({ status }) => status !== SelectionStatus.TAGGED}
             
         }
     }
 `
 
-const SelectionItem = styled.a`
+const SelectionItem = styled.div`
     font-size: 12px;
     color: #8E8E8E;
     vertical-align: center;
     letter-spacing: 1px;
     font-weight: 600;
-
 `
 
-const PostBox = styled.div`
-  width: 100%;
-  height: 70%;
-  margin: 0 auto;
-  
-// text-align: center;
-
-/* display: flex; */
-/* justify-content: center; */
-// flex-wrap: wrap;
-
-//   display: grid;
-//   grid-template-columns: repeat(auto-fill, minmax(30%, 1fr));
-//   grid-auto-rows: 20px;
-//   grid-gap: 40px;
-  
-`;
-
 const FeedBox = styled.div`
-    width: 100%;
-    height: 100%;
+    content-align: center;
+    width: 100%%;
+    height: 1000%;
     display: flex;
-
-    & div {
-        box-sizing: border-box;
-        width: 33.33333%;
-        //min-height: 100px;
-        margin-right:28px;
-
-        // &:last-of-type {
-        //     margin-right: 0px;
-        // }
-
-        &:nth-child(3n) {
-            margin-right: 0px;
-        }
-    }
+    justify-content: flex-start;   
 `
 
 const FeedImg = styled.img`
   //background-size: cover;
   //background-image: url("https://content.presspage.com/uploads/2379/1920_ocean-sunset-195865.jpg?10000");
-  content: url("https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Square_definition.svg/180px-Square_definition.svg.png");
-  width: 100%;
-  height: 100%;
+  content: url("https://static.hubzum.zumst.com/hubzum/2019/02/25/10/054889f91efb4d89abcb42317c298504_780x0c.jpg");
+  width: 280px;
+  height: 280px;
   object-fit: cover;
-
   cursor: pointer;
+  border-spacing: 0 20px 0 0;
+  border-width: 5px;
+  margin: 0 30px 0 0;
 `;
 
-const FollowContainer = styled.div`
-  font-size: 18px;
-  word-spacing: 10px;
-  
-`;
-
-// pop up
-const PopUpStatus = {
-    TRUE : true,
-    FALSE : false
-}
-
-
-function Profile() {
-
-    const [pageSelection, setPageSelection] = useState("posts");
-// pop up
-    const [editPopUp, setEditPopUp] = useState(false);
-
-// pop up
-    const handleEditPopUp = (isEnable) => {
-        if(isEnable === true) {
-            setEditPopUp(true);
-            document.body.style.overflow = "hidden";
-        }
-        else {
-            setEditPopUp(false);
-            document.body.style.overflow = "unset";
-        }
-/* 모달창 팝업시 뒷배경 스크롤 막기
-    https://velog.io/@kbing14/React-%EB%AA%A8%EB%8B%AC%EC%B0%BD-%ED%8C%9D%EC%97%85%EC%8B%9C-%EB%92%B7%EB%B0%B0%EA%B2%BD-%EC%8A%A4%ED%81%AC%EB%A1%A4-%EB%A7%89%EA%B8%B0
-*/
-    }
-
-    const handlePageSelection = (e) => {
-        // pageSelection을 posts로 바꾼다
-        if(e.target.textContent === "게시물")
-            setPageSelection("posts");
-        else if(e.target.textContent === "저장됨")
-            setPageSelection("saved");
-        else if(e.target.textContent === "태그됨")
-            setPageSelection("tagged");
-    }
-
-    return (<Layout>
-
-        <Wrapper id="Wrapper">
-            <Container id="Container">
-                <ProfileBox>
-                    <ProfileImg><div></div></ProfileImg>
-                    <ProfileName>
-                        <span> utae1102 </span>
-                        <Link to = {"/ProfileChange"}>
-                            <button  style={{fontSize: "15px"}}> 프로필 편집 </button>
-                        </Link>
-                        <span onClick={() => {handleEditPopUp(true)}}> ⚙︎ </span>
-                            {editPopUp && <PopUp onClose={handleEditPopUp} />}
-                        <FollowContainer>
-                            <span> 게시물5 </span>
-                            <span> 팔로워100 </span>
-                            <span> 팔로우100 </span>
-                        </FollowContainer>
-                        <p style={{ fontSize: "18px"}}>김우태</p> 
-                    </ProfileName>
-                </ProfileBox>
-                <SelectionBox>
-                    <div status={pageSelection}><SelectionItem onClick={handlePageSelection}>게시물</SelectionItem></div>
-                    <div status={pageSelection}><SelectionItem onClick={handlePageSelection}>저장됨</SelectionItem></div>
-                    <div status={pageSelection}><SelectionItem onClick={handlePageSelection}>태그됨</SelectionItem></div>
-                </SelectionBox>
-                {
-                    (pageSelection === SelectionStatus.POSTS)
-                    &&
-                        <PostBox>
-                            <FeedBox>
-                                <div><FeedImg/></div>
-                                <div><FeedImg/></div>
-                                <div><FeedImg/></div>
-                            </FeedBox>
-                            <FeedBox>
-                                <div><FeedImg/></div>
-                                <div><FeedImg/></div>
-                            </FeedBox>
-                            
-                        </PostBox>
-                    ||  (pageSelection === SelectionStatus.SAVED)
-                        && <div> saved </div>
-                    ||  (pageSelection === SelectionStatus.TAGGED)
-                        && <div> tagged </div>
-                }
-        </Container> 
-       </Wrapper>
-       <Footer/>
-    </Layout>)
-    
-   
-  
-  
-}
-
-export default Profile;
+const NewCollection = styled.div`
+    font-size: 14px;
+    font-weight: 550;
+    color: #0095f6;
+    cursor: pointer;
+    float: right;
+    margin: -8px 0 0 0;
+`
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
 const FooterStyle = styled.div`
- text-align: center;
-
- background-color:  #ffffff;
+    width: 100%;
+    height: 80px;
+    padding: 0px 16px;
+    @media (max-width: 850px) {
+        height: 105px;
+    }
+    margin-bottom: auto;
+    text-align: center;
+    background-color: #fafafa;
+    line-height: 20px;
+    margin-top: 20px;
 `;
 
+const FooterItem = styled.div`
+    display: inline-block;
+    margin: 0 8px 12px 8px;
+    font-size : 12.1px;
+    color: gray;
+    cursor: pointer;
+`
+
 function Footer() {
-    return <div>
-    <FooterStyle>
-      Meta 소개 블로그 채용 정보 도움말 API 개인정보처리방침 약관 인기 계정 해시태그 위치 Instagram Lite 연락처 업로드 & 비사용자
-    </FooterStyle>
-    </div>
-  };
+    return ( <FooterStyle>
+        <FooterItem>Meta</FooterItem>
+        <FooterItem>소개</FooterItem>
+        <FooterItem>블로그</FooterItem>
+        <FooterItem>채용</FooterItem>
+        <FooterItem>정보</FooterItem>
+        <FooterItem>도움말</FooterItem>
+        <FooterItem>API</FooterItem>
+        <FooterItem>개인정보처리방침</FooterItem>
+        <FooterItem>약관</FooterItem>
+        <FooterItem>인기 계정</FooterItem>
+        <FooterItem>해시태그</FooterItem>
+        <FooterItem>위치</FooterItem>
+        <FooterItem>Instagram Lite</FooterItem>
+        <FooterItem>연락처 업로드 & 비사용자</FooterItem>
+        <br/>
+        <FooterItem>한국어</FooterItem>
+        <FooterItem style={{cursor: "text"}}>© 2022 Instagram from Meta</FooterItem>
+    </FooterStyle>);
+}
 
 export default Footer;
-
-
-// import Footer from "/Users/gim-utae/wootae/src/components/Footer.js"
